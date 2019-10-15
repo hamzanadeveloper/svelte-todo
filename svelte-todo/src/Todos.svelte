41544122f8e6f7945e3a554043f4eb3325477b22
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-10-15 09:40:09 -0400

    Created some seed data for the todo list

---
+++ svelte-todo/src/Todos.svelte
@@ -0,0 +1,25 @@
+<script>
+import TodoItem from './TodoItem.svelte';
+
+let todos = [
+    {
+        id: 1,
+        title: 'My first todo',
+        completed: false
+    },
+    {
+        id: 2,
+        title: 'My second todo',
+        completed: false
+    },
+    {
+        id: 3,
+        title: 'My third todo',
+        completed: false
+    },
+];
+
+let newTodoTitle ='';
+let currentFilter = 'all';
+let nextId = 4;
+</script>
