614d64b170a7131962ff6dd068ef1351de3ec1f2
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-10-15 09:42:20 -0400

    Front-facing todo component code

--- svelte-todo/src/Todos.svelte
+++ svelte-todo/src/Todos.svelte
@@ -23,3 +23,32 @@ let newTodoTitle ='';
 let currentFilter = 'all';
 let nextId = 4;
 </script>
+
+<div class="container">
+    <h2>Svelte Todo App
+
+    <input type="text" class="todo-input" placeholder="Insert todo item ..." bind:value={newTodoTitle} on:keydown={addTodo}>
+
+    {#each filteredTodos as todo}
+        <div class="todo-item">
+            <TodoItem {...todo} on:deleteTodo={handleDeleteTodo} on:toggleComplete={handleToggleComplete} />
+        </div>
+    {/each}
+
+    <div class="inner-container">
+        <div><label><input class="inner-container-input" type="checkbox" on:change={checkAllTodos}>Check All</label></div>
+        <div>{todosRemaining} items left</div>
+    </div>
+
+    <div class="inner-container">
+        <div>
+            <button on:click={() => updateFilter('all')} class:active="{currentFilter === 'all'}">All</button>
+            <button on:click={() => updateFilter('active')} class:active="{currentFilter === 'active'}">Active</button>
+            <button on:click={() => updateFilter('completed')} class:active="{currentFilter === 'completed'}">Completed</button>
+        </div>
+        <div>
+            <button on:click={clearCompleted}>Clear Completed</button>
+        </div>
+    </div>
+
+</div>
